49e1c72f71ef36b543e15d3363c8fe0ef6cba432
notgonnaposta <125107572+notgonnaposta@users.noreply.github.com>
2025-05-27 00:38:22 -0400

    added a couple extra files

---
+++ Assets/Scripts/hide pref.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using UnityEngine;
+
+public class hidepref : MonoBehaviour
+{
+    public GameObject mainmenu;
+    public GameObject settingsmenu;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void preftomenu()
+    {
+        mainmenu.SetActive(true);
+        settingsmenu.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/quality changer.cs
@@ -0,0 +1,42 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class qualitychanger : MonoBehaviour
+{
+    public Dropdown qualityDropdown;
+    private const string PREF_KEY = "QualitySetting";
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        qualityDropdown.ClearOptions();
+
+        string[] names = QualitySettings.names;
+        qualityDropdown.AddOptions(new System.Collections.Generic.List<string>(names));
+
+        int savedIndex = PlayerPrefs.GetInt(PREF_KEY, QualitySettings.GetQualityLevel());
+        qualityDropdown.value = savedIndex;
+        qualityDropdown.RefreshShownValue();
+        
+        ApplyQuality(savedIndex);
+
+        qualityDropdown.onValueChanged.AddListener(delegate { OnDropdownChanged(qualityDropdown.value); });
+    }
+    void OnDropdownChanged(int index)
+    {
+ApplyQuality(index);
+PlayerPrefs.SetInt(PREF_KEY, index);
+PlayerPrefs.Save();
+    }
+    void ApplyQuality(int index)
+    {
+        QualitySettings.SetQualityLevel(index, true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
