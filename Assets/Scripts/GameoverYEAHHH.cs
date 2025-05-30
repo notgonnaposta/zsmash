16904e61d8afff146ba674a5aa503508a4a34832
notgonnaposta <125107572+notgonnaposta@users.noreply.github.com>
2025-05-30 05:28:46 -0400

    added new pause screen!

--- Assets/Scripts/GameoverYEAHHH.cs
+++ Assets/Scripts/GameoverYEAHHH.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class GameoverYEAHHH : MonoBehaviour
 {
@@ -6,9 +7,19 @@ public class GameoverYEAHHH : MonoBehaviour
     public Damage damage;
     public pause pause;
     public bool latch = false;
+    public GameObject numberui;
+    public AudioSource GOmusic;
+    public AudioSource IGmusic;
+    public float dB;
+    public AudioMixer SFX;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        float savedNormalized = PlayerPrefs.GetFloat("Volume_SFXvolume", 1f);
+
+
+         dB = (savedNormalized > 0.0001f) ? Mathf.Log10(savedNormalized) * 20f : -80f;
+        
         
     }
 
@@ -18,11 +29,14 @@ public class GameoverYEAHHH : MonoBehaviour
         if (damage.health == 0 && latch == false)
         {
 
-
+            numberui.SetActive(false);
             Gameoverscreen.SetActive(true);
             Time.timeScale = 0;
-            AudioListener.pause = true;
+            
             latch = true;
+            IGmusic.Pause();
+        GOmusic.Play();
+            SFX.SetFloat("SFXvolume", -80f);
         }
         
     }
--- Assets/Scripts/pause.cs
+++ Assets/Scripts/pause.cs
@@ -1,19 +1,39 @@
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class pause : MonoBehaviour
 {
     public GameObject Pausescreen;
+    public GameObject numberui;
+    public AudioSource IGmusic;
+    public AudioSource PSmusic;
+    public AudioMixer SFX;
+    public float dB;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void Pause()
     {
+        float savedNormalized = PlayerPrefs.GetFloat("Volume_SFXvolume", 1f);
+
+
+         dB = (savedNormalized > 0.0001f) ? Mathf.Log10(savedNormalized) * 20f : -80f;
+        IGmusic.Pause();
+        PSmusic.Play();
+        SFX.SetFloat("SFXvolume", -80f);
+
         Time.timeScale = 0;
-        AudioListener.pause = true;
+        numberui.SetActive(false);
         Pausescreen.SetActive(true);
     } 
     public void unpause()
     {
+        IGmusic.UnPause();
+        PSmusic.time = 0f;
+        PSmusic.Pause();
+       SFX.SetFloat("SFXvolume", dB);
+
         Time.timeScale = 1;
         AudioListener.pause = false;
+        numberui.SetActive(true);
         Pausescreen.SetActive(false);
     }
 
